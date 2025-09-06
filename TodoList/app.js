16e87727ab5b7d5b248c22a059bcd34dffec0c58
Max-kleb <maximesonke06@gmail.com>
2025-09-06 09:16:53 +0100

    hamburger-switch Co-authored-by: Backslash <backslash057@users.noreply.github.com>

--- TodoList/app.js
+++ TodoList/app.js
@@ -1,12 +1,29 @@
 let revelButton = document.querySelector(".reveal");
 let revealIcon = document.querySelector(".reveal-icon");
+let hamburgerButton = document.querySelector(".hamburger");
 
 let listsContainer = document.querySelector(".lists-container");
 
+let sidebar = document.querySelector(".sidebar");
+
+
+ console.log("Sidebar:", sidebar);
+ console.log("Hamburger button:", hamburgerButton);
 
 revelButton.addEventListener("click", (event) => {
     event.preventDefault();
 
     listsContainer.classList.toggle("hidden");
     revealIcon.classList.toggle("arrow-up");
+
 });
+
+
+hamburgerButton.addEventListener("click" , (event) => {
+    event.preventDefault();
+
+    sidebar.classList.toggle("hide");
+    
+    console.log("Classes de la sidebar:", sidebar.classList);
+    console.log("Style display:", window.getComputedStyle(sidebar).display);
+} ) ;
